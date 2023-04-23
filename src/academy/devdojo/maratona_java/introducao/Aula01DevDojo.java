527b2dc8cea19b3df0f373e8a48b7e0ce201b290
Guilherme <guilhermebarradasdev@gmail.com>
2023-04-23 01:51:43 -0300

    aula01 e aula02

---
+++ src/academy/devdojo/maratona_java/introducao/Aula01DevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class Aula01DevDojo {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo");
+    }
+}
---
+++ src/academy/devdojo/maratona_java/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        System.out.println("Tipos primitivos são \nint, float, double, char, byte, short, long e boolean");
+        int age = 21;
+        System.out.println("Minha idade é: " + age + " anos.");
+    }
+}
